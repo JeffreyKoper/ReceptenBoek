d37b9dd78e134a0c747ee989dac764e04b45d61a
Jeffrey Koper <jkoper257@student.novacollege.nl>
2023-03-22 09:28:07 +0100

    Connectie met de database is gemaakt

--- site/index.php
+++ site/index.php
@@ -1,5 +1,19 @@
 <?php
-
+require 'database.php';
 // Dit is het startpunt van je applicatie.
 
-echo 'Welkom bij het receptenboek';
+$hamburger = "images/Hamburger.png";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="<?php echo $hamburger ?>" alt="">
+</body>
+</html>
