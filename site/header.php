e0df54c0c7ea3bcf74e15dbcac01724dfd424aaa
Jeffrey Koper <jkoper257@student.novacollege.nl>
2023-03-22 13:32:45 +0100

    Header Geupdated & bepaalde CSS classen aangepast

--- site/header.php
+++ site/header.php
@@ -1,4 +1,19 @@
+<?php 
+    require 'database.php';
+
+  
+    $stmt = $conn->prepare("SELECT COUNT(id) as totaalRecepten FROM `Recepten`");
+    $stmt->execute();
+  
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $aantalRecepten = $stmt->fetch();
+
+    var_dump($aantalRecepten)
+  
+?>
+
 <link rel="stylesheet" href="css/style.css">
 <header>
-  <h1>Amerikaans Receptenboek</h1>
+  <h1>Amerikaans Receptenboek <div class="floatRechts">  Totaal Aantal Recepten: <?php echo $aantalRecepten['totaalRecepten'] ?></div> </h1>
 </header>
--- site/nav.php
+++ site/nav.php
@@ -4,6 +4,6 @@
     <li><a href="index.php">Home</a></li>
     <li><a href="#">Recepten</a></li>
     <li><a href="#">Ingredienten</a></li>
-    <li class="inloggen"><a href="#">Inloggen/Registreren</a></li>
+    <li class="floatRechts"><a href="#">Inloggen/Registreren</a></li>
   </ul>
 </nav>
