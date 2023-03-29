9a39af0584dd9fd90a07d434e12b4dd691f656ff
Jeffrey Koper <jkoper257@student.novacollege.nl>
2023-03-29 11:41:56 +0200

    CSS iets verbeterd

---
+++ site/specials.php
@@ -0,0 +1,34 @@
+<?php
+require 'database.php';
+
+session_start();
+
+$stmt = $conn->prepare("SELECT * FROM `Recepten` ORDER BY `tijdsduur` DESC");
+$stmt->execute();
+  
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $recepten = $stmt->fetchAll();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Recepten</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <div class="recepten-container">
+        <?php foreach ($recepten as $recept) : ?>
+            <div class="recepten-items">
+                <h1><?php echo $recept['gerecht_naam']?> </h1>
+                <img height="200px" width="200px" src="images/<?php echo $recept['afbeelding'] ?>" alt="een foto van <?php echo $recept['gerecht_naam'] ?>">
+                <a href="recept.php?id=<?php echo $recept["id"] ?>"><button type="submit" class="detail-buttons">Klik hier voor meer details!</button></a>
+            </div>
+        <?php endforeach; ?>
+        </div>
+    <?php include 'footer.php'; ?>
